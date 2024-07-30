28cf9fff2a743eede40836661894545835929d8d
lencov <lenox.covington@gmail.com>
2024-07-29 15:24:43 -1000

    beetle counts now save to an excel file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PestClassifierModel
-ML model for identifying invasive pests in near-infrared hyperspectral images
+Machine Learning for classifying species in near-infrared hyperspectral images
+
+When running beetleCounter.py, make sure that the xlsx file is not open in another application because then you will not have permission to edit it
+
+    The LDA_BeetleClassiferV1_with_CBB(I should change the name) model is optimized for 5 different beetle species. To train a model to be able to classify different pest species,
+you will need to provide
+
+will finish writing this later
--- src/visualization/beetleCounter.py
+++ src/visualization/beetleCounter.py
@@ -4,13 +4,44 @@ from spectral import open_image
 import joblib
 from scipy.ndimage import label
 from scipy.stats import mode
+import pandas as pd
+import os
+
+def update_spreadsheet(file_path, new_entry):
+    """
+    Update the spreadsheet with the new beetle counts and file path.
+
+    :param file_path: Path to the spreadsheet
+    :param new_entry: Dictionary containing the beetle counts and file path
+    """
+    # Check if the spreadsheet exists
+    if os.path.exists(file_path):
+        df = pd.read_excel(file_path, index_col=0)
+    else:
+        # Create a new DataFrame if the spreadsheet does not exist
+        columns = ['CRYPH', 'ERUD', 'AND', 'TNB', 'CBB', 'File Path']
+        df = pd.DataFrame(columns=columns)
+
+    # Append the new entry to the DataFrame
+    new_entry_df = pd.DataFrame([new_entry])
+    df = pd.concat([df, new_entry_df], ignore_index=True)
+
+    # Calculate the total counts
+    total_counts = df.sum(numeric_only=True)
+    total_counts['File Path'] = 'Total'
+
+    # Insert the total counts at the top of the DataFrame
+    df = pd.concat([total_counts.to_frame().T, df], ignore_index=True)
+
+    # Save the updated DataFrame to the spreadsheet
+    df.to_excel(file_path)
 
 # Load the trained model
 model_save_path = r'C:\Users\Headwall\Desktop\PestClassifier\models\LDA_BeetleClassifierV1_with_CBB.pkl'
 model = joblib.load(model_save_path)
 
 # Load the hyperspectral image. Make sure that within the same directory, there exists the data binary files. The other files might be required as well so just make sure all the files you get from taking the image are present
-hdr_path = r'C:\Users\Headwall\Desktop\PestClassifier\data\raw\AND\newImage4\data.hdr'
+hdr_path = r'C:\Users\Headwall\Desktop\PestClassifier\data\raw\CBB\data.hdr'
 img = open_image(hdr_path)
 data = img.load()
 
@@ -71,5 +102,10 @@ for i in range(1, num_features + 1):
 
 print("Beetle counts:", beetle_counts)
 
-# Save the predicted labels if needed
-#np.savetxt('predicted_labels.csv', predicted_labels, delimiter=',')
+# Prepare the new entry for the spreadsheet
+new_entry = beetle_counts.copy()
+new_entry['File Path'] = hdr_path
+
+# Update the spreadsheet
+spreadsheet_path = r'C:\Users\Headwall\Desktop\PestClassifier\beetle_counts.xlsx'
+update_spreadsheet(spreadsheet_path, new_entry)
